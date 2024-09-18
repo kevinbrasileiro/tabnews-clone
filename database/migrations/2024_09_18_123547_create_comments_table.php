1c0dc95d26d1056a39f2c0beef1d3e4e9f67114a
Kevin Brasilleiro <kevin.brasileiro08@gmail.com>
2024-09-18 09:44:33 -0300

    feat: add core models & migrations

---
+++ database/migrations/2024_09_18_123547_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Comment::class)->nullable(); // TODO: see if this works
+            $table->string('body');
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Post::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
