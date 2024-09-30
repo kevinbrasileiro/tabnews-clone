809e607a2580d3c66374c05d9c0fb3280803a015
Kevin Brasilleiro <kevin.brasileiro08@gmail.com>
2024-09-30 17:01:22 -0300

    feat: add old values to edit post form

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,8 +2,8 @@
     <x-forms.form method="POST" action="/{{$post->user->username}}/{{$post->slug}}">
         @method('PATCH')
         <x-page-header>Edit Post</x-page-header>
-        <x-forms.input label="Title" name="title" value="{{$post->title}}"/>
-        <x-forms.textarea-edit rows="20" label="Content" name="body" :editContent="$post->body"/>
+        <x-forms.input label="Title" name="title" value="{{ old('title', $post->title) }}"/>
+        <x-forms.textarea-edit rows="20" label="Content" name="body" editContent="{{ old('body', $post->body) }}"/>
         <div class="flex justify-between items-center">
             <x-forms.button>Edit Post</x-forms.button>
         </div>
